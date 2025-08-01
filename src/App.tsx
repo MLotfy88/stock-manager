f5eccf581f531e278c8283ae882e013472fca576
MLotfy88 <m.m.lotfy.88@gmail.com>
2025-08-02 02:19:50 +0300

    Changes to be committed: 	modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,10 +12,6 @@ import AddInventoryPage from './pages/AddSupplyPage';
 import CalendarPage from './pages/CalendarPage';
 import AlertsPage from './pages/AlertsPage';
 import ReportsPage from './pages/ReportsPage';
-import ManufacturersPage from './pages/ManufacturersPage';
-import SuppliersPage from './pages/SuppliersPage';
-import ProductDefinitionsPage from './pages/SupplyTypesPage';
-import StoresPage from './pages/StoresPage';
 import TransferInventoryPage from './pages/TransferInventoryPage';
 import InventoryReportPage from './pages/InventoryReportPage';
 import ReorderPointReportPage from './pages/ReorderPointReportPage';
@@ -41,10 +37,6 @@ function App() {
             <Route path="/inventory-report" element={<InventoryReportPage />} />
             <Route path="/reorder-point-report" element={<ReorderPointReportPage />} />
             <Route path="/consumption-report" element={<ConsumptionReportPage />} />
-            <Route path="/manufacturers" element={<ManufacturersPage />} />
-            <Route path="/suppliers" element={<SuppliersPage />} />
-            <Route path="/supply-types" element={<ProductDefinitionsPage />} />
-            <Route path="/stores" element={<StoresPage />} />
             <Route path="/transfer-inventory" element={<TransferInventoryPage />} />
             <Route path="/import-export" element={<ImportExportPage />} />
             <Route path="/management" element={<ManagementPage />} />
