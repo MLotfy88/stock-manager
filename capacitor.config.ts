55bdcf3c00827d136ddbfba30696463fcb7fed07
MLotfy88 <m.m.lotfy.88@gmail.com>
2025-11-04 19:19:40 +0200

    Changes to be committed: 	modified:   capacitor.config.ts 	modified:   index.html

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.qasarah.manager',
-  appName: 'Qasarah Manager',
+  appId: 'com.stocky.app',
+  appName: 'Stocky',
   webDir: 'dist'
 };
 
